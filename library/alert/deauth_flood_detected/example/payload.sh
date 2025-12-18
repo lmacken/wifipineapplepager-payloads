485301b1156d022be1f8c1b85385727215d43eeb
Darren Kitchen <darren@hak5.org>
2025-12-18 14:26:11 -0600

    example alert

--- library/alert/deauth_flood_detected/example/payload.sh
+++ library/alert/deauth_flood_detected/example/payload.sh
@@ -1,1 +1,7 @@
-# placeholder - will be updated shortly
+#!/bin/bash
+# Title: Example Deauth Flood Alert
+# Description: Alert human readable summary
+# Author: Hak5Darren
+# Version: 1
+
+ALERT "$_ALERT_DENIAL_MESSAGE"
